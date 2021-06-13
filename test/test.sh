aba1e573cf16f348b67fac4f75c2428c96c36905
GuDimz <zgudimz@gmail.com>
2021-06-13 12:55:46 +0300

    test

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+echo "====================================================================================="
+
 echo "\n\n\e[1;31m  ########  #### ########  ######## ##     ##    ######## ########  ######  ######## \e[0m"
 echo "\e[1;32m  ##     ##  ##  ##     ## ##        ##   ##        ##    ##       ##    ##    ##        \e[0m"
 echo "\e[1;34m  ##     ##  ##  ##     ## ##         ## ##         ##    ##       ##          ##        \e[0m"
@@ -13,20 +15,20 @@ cd ./test
 
 ./pipex my_infile.txt "grep ?" "wc -w" my_outfile.txt
 
-< or_infile.txt grep "?" | wc -w > or_outfile.txt
+< or_infile.txt grep "?" | wc -w> or_outfile.txt
 
 echo "\n\n====================================================================================="
 echo "*************************************\e[1;32m TEST №1 \e[0m***************************************\n"
 
-echo "./pipex \e[1;32m my_infile.txt \e[0m \e[1;31m [grep ?] [wc -w] \e[0m \e[1;32m my_outfile.txt \e[0m\n"
-echo "\e[1;32m< or_infile.txt \e[1;31m [grep ?] | [wc -w] \e[0m \e[1;32m > or_outfile.txt\e[0m\n\n"
+echo "\e[1;34m./pipex \e[1;32m my_infile.txt \e[0m \e[1;31m [grep ?] [wc -w] \e[0m \e[1;32m my_outfile.txt \e[0m\n"
+echo "\e[1;32m< or_infile.txt \e[1;31m [grep ?] \e[1;32m | \e[1;31m [wc -w] \e[1;32m > or_outfile.txt\e[0m\n\n"
 diff or_outfile.txt my_outfile.txt
 
 if [ $? = 0 ]
 then
-	echo "my_outfile == or_outfile  \e[1;32m [ OK:) ] \e[0m"
+	echo "\e[1;37m my_outfile == or_outfile  \e[1;32m [ OK:) ] \e[0m"
 else
-	echo "my_outfile != or_outfile   \e[1;31m [ FAIL:( ] \e[0m"
+	echo "\n\e[1;37m my_outfile != or_outfile   \e[1;31m [ FAIL:( ] \e[0m"
 fi
 rm -rf my_outfile.txt
 rm -rf or_outfile.txt
@@ -40,15 +42,15 @@ rm -rf or_outfile.txt
 echo "\n\n====================================================================================="
 echo "*************************************\e[1;32m TEST №2 \e[0m***************************************\n"
 
-echo "./pipex \e[1;32m my_infile.txt \e[0m \e[1;31m [grep ?] [wc -w] [cat -e] [cat -e] \e[0m \e[1;32m my_outfile.txt \e[0m\n"
-echo "\e[1;32m< or_infile.txt \e[1;31m [grep ?] | [wc -w] [cat -e] [cat -e] \e[0m \e[1;32m > or_outfile.txt\e[0m\n\n"
+echo "\e[1;34m./pipex \e[1;32m my_infile.txt \e[0m \e[1;31m [grep ?] [wc -w] [cat -e] [cat -e] \e[0m \e[1;32m my_outfile.txt \e[0m\n"
+echo "\e[1;32m< or_infile.txt \e[1;31m [grep ?] \e[1;32m | \e[1;31m [wc -w] \e[1;32m | \e[1;31m [cat -e] \e[1;32m | \e[1;31m [cat -e] \e[1;32m > or_outfile.txt\e[0m\n\n"
 diff or_outfile.txt  my_outfile.txt
 
 if [ $? = 0 ]
 then
-	echo "my_outfile == or_outfile  \e[1;32m [ OK:) ] \e[0m"
+	echo "\e[1;37m my_outfile == or_outfile  \e[1;32m [ OK:) ] \e[0m"
 else
-	echo "my_outfile != or_outfile   \e[1;31m [ FAIL:( ] \e[0m"
+	echo "\n\e[1;37m my_outfile != or_outfile   \e[1;31m [ FAIL:( ] \e[0m"
 fi
 rm -rf my_outfile.txt
 rm -rf or_outfile.txt
